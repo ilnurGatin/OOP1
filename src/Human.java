573a907c0923c087e8465621ff194b212bedff60
ilnurGatin <reilag@yandex.ru>
2022-10-18 01:34:56 +0500

    ДЗ. Задания 1-2

--- src/Human.java
+++ src/Human.java
@@ -6,10 +6,25 @@ public class Human {
     String jobTitle;
 
     Human(String name, int yearOfBirth, String city, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
         this.city = city;
         this.name = name;
         this.jobTitle = jobTitle;
+    }
+
+        Human(int yearOfBirth) {
+        if (this.yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+        this.city = "'Информация не указана'";
+        this.name = "'Информация не указана'";
+        this.jobTitle = "'Информация не указана'";
     }
 
     void hello() {
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         Human maksim = new Human("Максим", 1987, "Минск", "бренд-менеджер");
         Human anna = new Human("Аня", 1993, "Москва", "методист образовательных программ");
-        Human kate = new Human("Катя", 1994, "Калининград", "продакт-менеджер");
-        Human artem = new Human("Артем", 1995, "Москва", "директор по развитию бизнеса");
+        Human kate = new Human("Катя", -1994, "Калининград", "продакт-менеджер");
+        Human artem = new Human(1995);
 
         maksim.hello();
         anna.hello();
